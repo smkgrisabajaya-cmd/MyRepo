e7b4c0ae7dafb14bce5e862bd5d0aa82d326bad2
smkgrisabajaya-cmd <smkgrisabajaya@gmail.com>
2026-09-09 22:05:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const ctx=document.getElementById("chart");
-new Chart(ctx,{
+const chart=document.getElementById("average");
+
+new Chart(chart,{
     type:"bar",
     data:{
-        labels:["Senin","Selasa","Rabu","Kamis"],
+        labels:["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"],
         datasets:[{
-            label:"pengunjung",
-            data:[12,19,8,15]
+            label:"Electricy Used",
+            data:[75,72,68,66,66,65,62]
         }]
+
+    options:{
+        scales:{
+            y:{
+                ticks{
+                callback:function(value){
+                    return value+"%";
+                }
+            }
+        }
     }
 });
