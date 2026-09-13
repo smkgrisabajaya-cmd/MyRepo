07c9958a565fcecd86ea359edfc2a32abf231251
smkgrisabajaya-cmd <smkgrisabajaya@gmail.com>
2026-09-14 05:49:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-const chart=document.getElementById("average");
+const chart = document.getElementById("average");
 
-new Chart(chart,{
-    type:"bar",
-    data:{
-        labels:["Senin","Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu"],
-        datasets:[{
-            label:"Electricy Used",
-            data:[75,72,68,66,66,65,62]
+new Chart(chart, {
+
+    type: "bar",
+
+    data: {
+        labels: [
+            "Senin",
+            "Selasa",
+            "Rabu",
+            "Kamis",
+            "Jumat",
+            "Sabtu",
+            "Minggu"
+        ],
+
+        datasets: [{
+            label: "Electricity Used",
+            data: [75, 72, 68, 66, 66, 65, 62],
+
+            backgroundColor: "#80e7c1",
+            borderRadius: 10,
+            borderColor: "#80e7c1"
         }]
+    },
 
-    options:{
-        scales:{
-            y:{
-                ticks{
-                callback:function(value){
-                    return value+"%";
+    options: {
+
+        responsive: true,
+        maintainAspectRatio: false,
+
+        scales: {
+
+            x: {
+                // Tidak perlu callback %
+            },
+
+            y: {
+                ticks: {
+                    callback: function(value) {
+                        return value + "%";
+                    }
                 }
             }
+
+        },
+
+        plugins: {
+
+            tooltip: {
+                callbacks: {
+
+                    label: function(context) {
+                        return context.dataset.label + ": " + context.raw + "%";
+                    }
+
+                }
+            }
+
         }
+
     }
+
 });
